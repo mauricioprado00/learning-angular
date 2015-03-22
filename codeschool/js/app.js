595c56da3bf2ecbd9caefb02c9f6e625aece5c60
Mauricio Prado <mauricioprado00@gmail.com>
2015-03-21 22:30:36 -0300

    ng-submit

--- codeschool/js/app.js
+++ codeschool/js/app.js
@@ -20,6 +20,15 @@
 		};
 	});
 
+	app.controller('ReviewController', function () {
+		this.review = {};
+
+		this.addReview = function(product) {
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
 
 	var gem = {
 		name: "Dodecahedron",
